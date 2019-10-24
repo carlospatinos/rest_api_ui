4aefabbc7778185ffe0fde89ecfb7a9c38f52e4c
Carlos Patino <carlos.patino_soria@genesys.com>
2019-10-24 08:04:12 +0100

    Fixing the hello.js to use remorte service

--- public/hello.js
+++ public/hello.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $.ajax({
-        url: "http://localhost:8080/showmyname"
+        url: "https://say-my-name-carlos.herokuapp.com/showmyname"
     }).then(function(data, status, jqxhr) {
        $('.greeting-id').append(data.id);
        $('.greeting-content').append(data.content);
